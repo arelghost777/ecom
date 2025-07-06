63ac0b7691e32037799d1fdd3134c8aec713fb50
arelghost777 <arelghost@gmail.com>
2025-07-06 05:48:24 +0100

    seckn commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,7 @@ export default function Home() {
   return (
     <>
       <Navbar/>
-      <HeroSlider/>
-      <Category/>
-      <BestSellers/>
-      <WhyChooseUs/>
-      <Newsletter/>
-      <Footer/>
+
     </>
   );
 }
